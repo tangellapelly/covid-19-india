5069a3a63211f4087c594e7b6e6d156657ca1496
Sudheer <ext-sudheer.tangellapelly@here.com>
2020-04-05 14:05:51 +0530

    added test centers data

---
+++ covid-19-testcenter-india.js
@@ -0,0 +1,443 @@
+[
+    {
+        "country": "India",
+        "state": "Delhi",
+        "test_center": "All India Institute Medical Sciences (AIIMS)",
+        "lat": 28.62623,
+        "lng": 77.21808
+    },
+    {
+        "country": "India",
+        "state": "Delhi",
+        "test_center": "National Centre for Disease Control (NCDC)",
+        "lat": 28.981005,
+        "lng": 77.250787
+    },
+    {
+        "country": "India",
+        "state": "MAHARASHTRA",
+        "test_center": "Indira Gandhi Government Medical College, Nagpur",
+        "lat": 21.237643,
+        "lng": 79.082249
+    },
+    {
+        "country": "India",
+        "state": "MAHARASHTRA",
+        "test_center": "Kasturba Hospital for Infectious Diseases, Mumbai",
+        "lat": 18.989135,
+        "lng": 72.828229
+    },
+    {
+        "country": "India",
+        "state": "RAJASTHAN",
+        "test_center": "Sawai Man Singh, Jaipur",
+        "lat": 26.905708,
+        "lng": 75.815528
+    },
+    {
+        "country": "India",
+        "state": "RAJASTHAN",
+        "test_center": "Dr. S.N Medical College, Jodhpur",
+        "lat": 26.269595,
+        "lng": 73.00743
+    },
+    {
+        "country": "India",
+        "state": "RAJASTHAN",
+        "test_center": "Jhalawar Medical College, Jhalawar",
+        "lat": 24.586077,
+        "lng": 76.161092
+    },
+    {
+        "country": "India",
+        "state": "RAJASTHAN",
+        "test_center": "SP Med. College, Bikaner",
+        "lat": 28.006645,
+        "lng": 73.330536
+    },
+    {
+        "country": "India",
+        "state": "RAJASTHAN",
+        "test_center": "RNT Medical College, Udaipur",
+        "lat": 24.588449,
+        "lng": 73.695066
+    },
+    {
+        "country": "India",
+        "state": "TAMIL NADU",
+        "test_center": "King's Institute of Preventive Medicine & Research, Chennai",
+        "lat": 13.012671,
+        "lng": 80.217589
+    },
+    {
+        "country": "India",
+        "state": "TAMIL NADU",
+        "test_center": "Government Medical College, Theni",
+        "lat": 10.006436,
+        "lng": 77.553715
+    },
+    {
+        "country": "India",
+        "state": "TAMIL NADU",
+        "test_center": "Tirunelveli Medical College, Tirunelveli",
+        "lat": 8.712006,
+        "lng": 77.75086
+    },
+    {
+        "country": "India",
+        "state": "TAMIL NADU",
+        "test_center": "Govt. Medical college, Thiruvaru",
+        "lat": 10.776296,
+        "lng": 79.603669
+    },
+    {
+        "country": "India",
+        "state": "UTTAR PRADESH",
+        "test_center": "King's George Medical University, Lucknow",
+        "lat": 26.86945,
+        "lng": 80.916229
+    },
+    {
+        "country": "India",
+        "state": "UTTAR PRADESH",
+        "test_center": "Institute of Medical Sciences, Banaras Hindu University, Varanasi",
+        "lat": 25.274707,
+        "lng": 82.999525
+    },
+    {
+        "country": "India",
+        "state": "UTTAR PRADESH",
+        "test_center": "Jawaharlal Nehru Medical College, Aligarh",
+        "lat": 27.918898,
+        "lng": 78.085708
+    },
+    {
+        "country": "India",
+        "state": "UTTARAKHAND",
+        "test_center": "Government Medical College, Haldwani",
+        "lat": 29.206559,
+        "lng": 79.521372
+    },
+    {
+        "country": "India",
+        "state": "WEST BENGAL",
+        "test_center": "National Institute of Cholera and Enteric Diseases, Kolkata",
+        "lat": 22.566125,
+        "lng": 88.399827
+    },
+    {
+        "country": "India",
+        "state": "WEST BENGAL",
+        "test_center": "IPGMER, Kolkata",
+        "lat": 22.539769,
+        "lng": 88.341862
+    },
+    {
+        "country": "India",
+        "state": "MADHYA PRADESH",
+        "test_center": "All India Institute Medical Sciences, Bhopal",
+        "lat": 23.207034,
+        "lng": 77.460151
+    },
+    {
+        "country": "India",
+        "state": "MADHYA PRADESH",
+        "test_center": "National Institute of Research in Tribal Health (NIRTH), Jabalpur",
+        "lat": 23.147418,
+        "lng": 79.878832
+    },
+    {
+        "country": "India",
+        "state": "KERALA",
+        "test_center": "National Institute of Virology Field Unit",
+        "lat": 18.545864,
+        "lng": 73.789489
+    },
+    {
+        "country": "India",
+        "state": "KERALA",
+        "test_center": "Govt. Medical College, Thriuvananthapuram",
+        "lat": 8.523644,
+        "lng": 76.928371
+    },
+    {
+        "country": "India",
+        "state": "KERALA",
+        "test_center": "Govt. Medical College, Kozhikhode",
+        "lat": 11.272611,
+        "lng": 75.837198
+    },
+    {
+        "country": "India",
+        "state": "KERALA",
+        "test_center": "Govt. Medical College, Thrissur",
+        "lat": 10.614729,
+        "lng": 76.196121
+    },
+    {
+        "country": "India",
+        "state": "KARNATAKA",
+        "test_center": "Bangalore Medical College & Research Institute, Bangalore",
+        "lat": 12.959657,
+        "lng": 77.574743
+    },
+    {
+        "country": "India",
+        "state": "KARNATAKA",
+        "test_center": "National Institute of Virology Field Unit Bangalore",
+        "lat": 12.937579,
+        "lng": 77.590805
+    },
+    {
+        "country": "India",
+        "state": "KARNATAKA",
+        "test_center": "Mysore Medical College & Research Institute, Mysore",
+        "lat": 12.315557,
+        "lng": 76.650523
+    },
+    {
+        "country": "India",
+        "state": "KARNATAKA",
+        "test_center": "Hassan Inst. of Med. Sciences, Hassan",
+        "lat": 13.005094,
+        "lng": 76.102676
+    },
+    {
+        "country": "India",
+        "state": "KARNATAKA",
+        "test_center": "Shimoga Inst. of Med. Sciences, Shivamogga",
+        "lat": 13.932569,
+        "lng": 75.566874
+    },
+    {
+        "country": "India",
+        "state": "Gujarat",
+        "test_center": "BJ Medical College, Ahmedabad",
+        "lat": 23.052599,
+        "lng": 72.602889
+    },
+    {
+        "country": "India",
+        "state": "Gujarat",
+        "test_center": "M.P.Shah Government Medical College, Jamnagar",
+        "lat": 22.478434,
+        "lng": 70.065862
+    },
+    {
+        "country": "India",
+        "state": "HARYANA",
+        "test_center": "BPS Govt Medical College, Sonipat",
+        "lat": 29.152095,
+        "lng": 76.80841
+    },
+    {
+        "country": "India",
+        "state": "HARYANA",
+        "test_center": "Pt. B.D. Sharma Post Graduate Inst. of Med. Sciences, Rohtak",
+        "lat": 28.880568,
+        "lng": 76.605654
+    },
+    {
+        "country": "India",
+        "state": "HIMACHAL PRADESH",
+        "test_center": "Indira Gandhi Medical College, Shimla, Himachal Pradesh",
+        "lat": 31.10677,
+        "lng": 77.18223
+    },
+    {
+        "country": "India",
+        "state": "HIMACHAL PRADESH",
+        "test_center": "Dr. Rajendra Prasad Govt. Med. College, Kangra, Tanda",
+        "lat": 32.099359,
+        "lng": 76.298688
+    },
+    {
+        "country": "India",
+        "state": "JAMMU AND KASHMIR",
+        "test_center": "Sher-e- Kashmir Institute of Medical Sciences, Srinagar",
+        "lat": 34.136305,
+        "lng": 74.800036
+    },
+    {
+        "country": "India",
+        "state": "JAMMU AND KASHMIR",
+        "test_center": "Government Medical College, Jammu",
+        "lat": 32.735844,
+        "lng": 74.854577
+    },
+    {
+        "country": "India",
+        "state": "JAMMU AND KASHMIR",
+        "test_center": "Government Medical College, Srinagar",
+        "lat": 34.086205,
+        "lng": 74.799295
+    },
+    {
+        "country": "India",
+        "state": "JHARKHAND",
+        "test_center": "MGM Medical College, Jamshedpur",
+        "lat": 22.843634,
+        "lng": 86.232417
+    },
+    {
+        "country": "India",
+        "state": "MEGHALAYA",
+        "test_center": "NEIGRI of Health and Medical Sciences, Shillong",
+        "lat": 25.5907284,
+        "lng": 91.9398369
+    },
+    {
+        "country": "India",
+        "state": "MANIPUR",
+        "test_center": "J N Inst. of Med. Sciences Hospital, Imphal-East, Manipur",
+        "lat": 24.809946,
+        "lng": 93.961123
+    },
+    {
+        "country": "India",
+        "state": "MANIPUR",
+        "test_center": "Regional Institute of Medical Sciences, Imphal",
+        "lat": 24.816065,
+        "lng": 93.922762
+    },
+    {
+        "country": "India",
+        "state": "ODISHA",
+        "test_center": "Regional Medical Research Center, Bhubaneswar",
+        "lat": 20.31675,
+        "lng": 85.819526
+    },
+    {
+        "country": "India",
+        "state": "PUNJAB",
+        "test_center": "Government Medical College, Patiala",
+        "lat": 30.328023,
+        "lng": 76.385115
+    },
+    {
+        "country": "India",
+        "state": "PUNJAB",
+        "test_center": "Government Medical College, Amritsar",
+        "lat": 31.653973,
+        "lng": 74.885225
+    },
+    {
+        "country": "India",
+        "state": "PUDUCHERRY",
+        "test_center": "Jawaharlal Institute of Postgraduate Medical Education & Research, Puducherry",
+        "lat": 11.950442,
+        "lng": 79.799707
+    },
+    {
+        "country": "India",
+        "state": "TELANGANA",
+        "test_center": "Gandhi Medical College, Secunderabad",
+        "lat": 17.424777,
+        "lng": 78.504389
+    },
+    {
+        "country": "India",
+        "state": "TELANGANA",
+        "test_center": "Osmania Medical College, Hyderabad",
+        "lat": 17.382246,
+        "lng": 78.482815
+    },
+    {
+        "country": "India",
+        "state": "TRIPURA",
+        "test_center": "Government Medical College, Agartala",
+        "lat": 23.85926,
+        "lng": 91.2913
+    },
+    {
+        "country": "India",
+        "state": "BIHAR",
+        "test_center": "Rajendra Memorial Research Institute of Medical Sciences, Patna",
+        "lat": 25.599784,
+        "lng": 85.197713
+    },
+    {
+        "country": "India",
+        "state": "CHANDIGARH",
+        "test_center": "Post Graduate Institute of Medical Education & Research, Chandigarh",
+        "lat": 30.765066,
+        "lng": 76.77505
+    },
+    {
+        "country": "India",
+        "state": "CHHATTISGARH",
+        "test_center": "All India Institute Medical Sciences, Raipur",
+        "lat": 21.25707,
+        "lng": 81.5792
+    },
+    {
+        "country": "India",
+        "state": "ANDAMAN and NICOBAR ISLANDS",
+        "test_center": "Regional Medical Research Centre, Port Blair, Andaman and Nicobar",
+        "lat": 11.635054,
+        "lng": 92.714616
+    },
+    {
+        "country": "India",
+        "state": "ANDHRA PRADESH",
+        "test_center": "Sri Venkateswara Institute of Medical Sciences, Tirupati",
+        "lat": 13.638011,
+        "lng": 79.403912
+    },
+    {
+        "country": "India",
+        "state": "ANDHRA PRADESH",
+        "test_center": "Andhra Medical College, Visakhapatnam",
+        "lat": 17.706585,
+        "lng": 83.304872
+    },
+    {
+        "country": "India",
+        "state": "ANDHRA PRADESH",
+        "test_center": "GMC, Anantapur",
+        "lat": 14.671255,
+        "lng": 77.592876
+    },
+    {
+        "country": "India",
+        "state": "ANDHRA PRADESH",
+        "test_center": "Sidhartha Medical College, Vijayawada",
+        "lat": 16.517553,
+        "lng": 80.671309
+    },
+    {
+        "country": "India",
+        "state": "ANDHRA PRADESH",
+        "test_center": "Rangaraya Medical College, Kakinada",
+        "lat": 16.979144,
+        "lng": 82.237245
+    },
+    {
+        "country": "India",
+        "state": "ASSAM",
+        "test_center": "Gauhati Medical College, Guwahati",
+        "lat": 26.154546,
+        "lng": 91.770113
+    },
+    {
+        "country": "India",
+        "state": "ASSAM",
+        "test_center": "Regional Medical Research Center, Dibrugarh",
+        "lat": 27.472762,
+        "lng": 94.982789
+    },
+    {
+        "country": "India",
+        "state": "ASSAM",
+        "test_center": "Silchar Medical College, Silchar",
+        "lat": 24.775824,
+        "lng": 92.794915
+    },
+    {
+        "country": "India",
+        "state": "ASSAM",
+        "test_center": "Jorhat Medical College, Jorhat",
+        "lat": 26.742603,
+        "lng": 94.195041
+    }
+]
